ddc8c5196a524e120e16473e7ceae211e8054d10
AdamHafidz <adamjuni2004@gmail.com>
2024-03-02 19:23:44 +0700

    landingpage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,7 +2,20 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <h1>kkonto;
-    </h1>
+    <div className="container mx-20 my-5 flex items-center">
+      <div className="max-w-[50%]">
+        <h1 className="text-5xl">
+          Menjadikan Pengantaran Barang Begitu Mudah
+        </h1>
+        <p className="text-lg my-4">
+          AndromedaMovers dapat membantu kamu untuk mengantar barang menjadi
+          lebih mudah.
+        </p>
+        <a className="bg-[#8b0000] text-white p-2 rounded-lg" href="https://wa.me/6287884393999">
+          Pesan Sekarang
+        </a>
+      </div>
+      <Image className="" width={700} height={700} src="/img/mobil.png" alt="" />
+    </div>
   );
 }
